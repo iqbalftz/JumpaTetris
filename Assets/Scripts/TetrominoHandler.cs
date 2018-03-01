230db01b7ad880f07b4bc039f7eadb82c432a57c
hiyasa <hhidayatul@rocketmail.com>
2018-03-01 17:06:29 +0700

    Deteksi batas tinggi tumpukan tetromino

--- Assets/Scripts/TetrominoHandler.cs
+++ Assets/Scripts/TetrominoHandler.cs
@@ -100,7 +100,10 @@ public class TetrominoHandler : MonoBehaviour
 
 			enabled = false;
 
-			gameplayManager.GenerateTetromino ();
+			if (gameplayManager.IsReactLimitGrid (this))
+				gameplayManager.GameOver (this);
+			else
+				gameplayManager.GenerateTetromino ();
 		}
 		else
 			gameplayManager.UpdateGrid (this);
